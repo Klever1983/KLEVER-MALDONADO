c804a66b34e75dfae834e2884277cb35fa7147ec
Klever1983 <kw.maldonadom@uea.edu.ec>
2026-01-17 09:42:23 -0500

    Add files via upload

---
+++ ListasEnlazadas/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class Nodo
+{
+    public int Dato;
+    public Nodo Siguiente;
+
+    public Nodo(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+class ListaEnlazada
+{
+    private Nodo cabeza;
+
+    public ListaEnlazada()
+    {
+        cabeza = null;
+    }
+
+    // EJERCICIO 1
+    public int ContarElementos()
+    {
+        int contador = 0;
+        Nodo actual = cabeza;
+
+        while (actual != null)
+        {
+            contador++;
+            actual = actual.Siguiente;
+        }
+
+        return contador;
+    }
+
+    // EJERCICIO 3
+    public int BuscarDato(int valor)
+    {
+        int contador = 0;
+        Nodo actual = cabeza;
+
+        while (actual != null)
+        {
+            if (actual.Dato == valor)
+                contador++;
+
+            actual = actual.Siguiente;
+        }
+
+        if (contador == 0)
+            Console.WriteLine("El dato no fue encontrado.");
+
+        return contador;
+    }
+
+    public void InsertarFinal(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+
+        if (cabeza == null)
+        {
+            cabeza = nuevo;
+        }
+        else
+        {
+            Nodo actual = cabeza;
+            while (actual.Siguiente != null)
+                actual = actual.Siguiente;
+
+            actual.Siguiente = nuevo;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+
+        lista.InsertarFinal(4);
+        lista.InsertarFinal(7);
+        lista.InsertarFinal(4);
+        lista.InsertarFinal(9);
+
+        Console.WriteLine("Cantidad de elementos: " + lista.ContarElementos());
+
+        int veces = lista.BuscarDato(4);
+        if (veces > 0)
+            Console.WriteLine("El dato se encontró " + veces + " veces.");
+    }
+}
---
+++ ListasEnlazadas/README.md
@@ -0,0 +1,12 @@
+
+# Listas Enlazadas en C#
+
+Este proyecto contiene la implementación de los siguientes ejercicios:
+
+## Ejercicio 1
+Función que calcula el número de elementos de una lista enlazada.
+
+## Ejercicio 3
+Método de búsqueda que retorna cuántas veces aparece un dato dentro de la lista enlazada.
+
+El proyecto está desarrollado en **C#**, utilizando listas enlazadas implementadas manualmente mediante nodos.
